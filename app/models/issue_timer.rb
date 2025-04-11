3e30cf79115be979c7e3065ba46f1ce0e25be06e
Никита Потапов <ns.potapov@yandex.ru>
2025-04-11 17:12:48 +0400

    refactor: clean project

--- app/models/issue_timer.rb
+++
@@ -1,8 +0,0 @@
-class IssueTimer < ApplicationRecord
-    def initialize
-        self.user_id = User.current.id
-        self.started_on = DateTime.now
-        self.time_spent = 0
-        self.paused = false
-      end
-end
