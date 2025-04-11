d91baa1ed9d700187b7ccc05f7c1c54324a21a3c
Никита Потапов <ns.potapov@yandex.ru>
2025-04-11 17:19:16 +0400

    fix hook

--- lib/redmine_timetracker_plugin/hooks.rb
+++ lib/redmine_timetracker_plugin/hooks.rb
@@ -1,6 +1,8 @@
 module RedmineTimetrackerPlugin
   class Hooks < Redmine::Hook::ViewListener
-    render_on(:view_layouts_base_body_bottom, :partial => 'redmine_timetracker_plugin/embed_menu')
+    def view_layouts_base_body_bottom(context)
+      javascript_include_tag('redmine_timetracker_plugin', plugin: :redmine_timetracker_plugin)
+    end
 
     def view_layouts_base_html_head(context)
       stylesheet_link_tag('redmine_timetracker_plugin.css', plugin: :redmine_timetracker_plugin)
